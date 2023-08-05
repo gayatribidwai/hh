312ff8bddf22a08e62cc1e34327fc5ac57ce6de0
gayatribidwai <gayatripbidwai0211@gmail.com>
2023-08-06 01:12:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Wallet_cart from './Components/Wallet_cart';
+
+function App() {
+  return (
+    // <Router>
+      <>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ display: 'inline-block' }}>NFT Marketplace</h1>
+      </div>
+      <Wallet_cart />
+        <Navbar />
+      
+        {/* <div className="container">
+          <Routes>
+            <Route path="/MintNFT" element={<MintNFT />} />
+            <Route path="/Buy_Nft" element={<Buy_Nft />} />
+            <Route path="/NFTList" element={<NFTList/>} />
+          </Routes>
+        </div> */}
+      </>
+    // </Router>
+  );
+}
+
+export default App;
+
+
+
+
+
